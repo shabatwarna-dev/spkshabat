4de63ad08b4ba7cdbc07a64f53c8c5282739de34
ianvndy <ashandyasr77.com>
2026-03-16 14:22:14 +0700

    fix proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
